1e44e53bbaf35cf6152ae15df4fae5c536003719
starenka <starenka0@gmail.com>
2020-03-07 10:10:59 +0100

    init db script

--- app.py
+++ app.py
@@ -73,5 +73,4 @@ def permalink(hash):
 
 
 if __name__ == '__main__':
-    # db.create_all()
     app.run(debug=settings.DEBUG)
---
+++ initdb.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+if __name__ == '__main__':
+    from app import db
+    db.create_all()
