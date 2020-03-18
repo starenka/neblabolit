ae34b7554dc6efa3a0814692323aff4eeb5a6d39
starenka <starenka0@gmail.com>
2020-03-18 22:26:18 +0100

    +korona

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from sqlalchemy.dialects.postgresql import JSON
 from generator import train_model, combined_model, generate
 import settings
 
-MODELS = dict(andrej=train_model('andrej'), mara=train_model('caulidi'))
+MODELS = dict(andrej=train_model('andrej'), mara=train_model('caulidi'),
+              andrej_korona=train_model('andrej_korona'))
 MODELS['andrej1mara0'] = combined_model((MODELS['andrej'], MODELS['mara']), (3, 1))
 MODELS['andrejmara'] = combined_model((MODELS['mara'], MODELS['andrej']), (1, 1))
 MODELS['mara1andrej0'] = combined_model((MODELS['mara'], MODELS['andrej']), (3, 1))
@@ -18,9 +19,9 @@ MODEL_MIXER = {1: MODELS['andrej'],
                2: MODELS['andrej1mara0'],
                3: MODELS['andrejmara'],
                4: MODELS['mara1andrej0'],
-               5: MODELS['mara']
+               5: MODELS['mara'],
+               100: MODELS['andrej_korona'],
                }
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB
@@ -61,16 +62,33 @@ def index():
                            opus=opus, hits=Opus.hits())
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html', title='Neni!'), 404
+@app.route('/korona')
+def korona():
+    model = MODELS['andrej_korona']
+
+    tlong = generate(model, items=20, separator=' ')
+    tshort = generate(model, items=12, separator='\n', max_chars=140)
+
+    opus = Opus(conf=dict(korona=True), text_long=tlong, text_short=tshort)
+    db.session.add(opus)
+    db.session.commit()
+
+    return render_template('korona.html', title='Koronavirusy',
+                           opus=opus, hits=Opus.hits())
 
 
 @app.route('/permalink/<hash>')
 def permalink(hash):
     opus = Opus.query.filter_by(id=hash).first_or_404()
-    return render_template('generator.html', title='Hlavně neblábolit',
+    template = 'generator' if not 'korona' in opus.conf else 'korona'
+    return render_template('%s.html' % template,
+                           title=min(opus.text_short.split('\n'), key=len),
                            opus=opus, hits=Opus.hits())
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='Neni!'), 404
 
 
 if __name__ == '__main__':
